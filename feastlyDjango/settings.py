70cf10afdb1e2f2ba8c382e2f9d6236e42fc1214
Maxwell Bates <maxbates@gmail.com>
2014-09-03 00:02:41 -0700

    add static files

--- feastlyDjango/settings.py
+++ feastlyDjango/settings.py
@@ -82,7 +82,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = '/Users/maxwellbates/feastlyDjango/com/static/'
+STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'
 
